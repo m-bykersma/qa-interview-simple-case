715881958bada6cbd8e0ca68984ce9302f482ee7
m-bykersma <bykersmam@gmail.com>
2025-04-03 13:25:08 +0200

    First attempt at code review. Updated login.tests with new locators, randomUser selection and modified redundant code

--- tests/login/login.test.ts
+++ tests/login/login.test.ts
@@ -1,29 +1,36 @@
 import { test, expect } from '@playwright/test'
 import { existingUsers } from '../../test-setup/localstorage.setup'
 
+// Modified Test Case - Login in with existing account
+
 test.describe.configure({ mode: 'serial' })
 
 test.describe('login form tests', () => {
   test('logging in works with existing account', async ({ page }) => {
-    await page.goto('localhost:8080/login')
+    // Changed page URL to explictly match output of npm run dev
+    await page.goto('http://localhost:8080/login')
 
-    const existingUser = existingUsers[0]
+    // Select a random user from the database and log user details to console
+    const randomUser = Math.floor(Math.random() * existingUsers.length)
+    console.log(randomUser, existingUsers[randomUser])
+    const existingUser = existingUsers[randomUser]
 
-    await page
-      .locator('#root form div:nth-child(1) > div > input')
-      .pressSequentially(existingUser.email)
+    // Wait for page and input email
+    // Changed presssequentially to fill and updated locator to id 
+    await page.locator('id=email').fill(existingUser.email)
 
-    await page
-      .locator('#root form div:nth-child(2) > div > input')
-      .pressSequentially(existingUser.password)
+    // Wait for page and input password
+    // Changed presssequentially to fill and updated locator to id
+      await page.locator('id=password').fill(existingUser.password)
 
-    // Submit button
-    const button = page.locator('form .MuiButton-sizeMedium')
-    // Click on the button
-    button.click()
+    // Click on the Login button
+    await page.locator('//button[contains(text(),"Login")]').click()
 
-    // Wait for 1 second until page is fully loaded
-    await page.waitForTimeout(1000)
-    await expect(page.getByText('Log out')).toBeVisible()
+    // Validate successful login. 
+    // Removed static wait, added timeout to expect and validates correct user login
+    await expect(page.getByText('Log out')).toBeVisible({timeout: 5000})
+    await expect(page.getByText(existingUsers[randomUser].firstName && existingUsers[randomUser].lastName)).toBeVisible({timeout: 5000})
+    // Take a screenshot for evidence and save to the screenshots folder
+    await page.screenshot({ path: 'screenshots/LoginTestScreenshot.png' });
   })
 })
