25dea3a3fefe70346333089530bac4a0ec0d32fd
m-bykersma <bykersmam@gmail.com>
2025-04-04 10:30:52 +0200

    Executed tests and include results

--- utilities/generated-data.ts
+++ utilities/generated-data.ts
@@ -1,3 +1,8 @@
 export const generatedUsers = [
-
+  {
+    "newUserFirstName": "frxkf",
+    "newUserLastName": "uqrrj",
+    "newUserEmail": "Z$Hb1743755231676@mail.com",
+    "newUserPassword": "yeRg@m8lHj"
+  }
 ];
